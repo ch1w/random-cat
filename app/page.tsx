1da89f0f00adc844e476e376fe9944ee6e1cd335
ch1w <a66266450@gmail.com>
2025-08-27 17:57:48 +0900

    あ

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { connection } from "next/server";
 import { fetchImage } from "./fetch-image"
 
 export default async function Home() {
-  await connection();
-  const image = await fetchImage();
+  await connection(); // ビルド時にfetchImageの結果が固定されないようにする
+  const image = await fetchImage(); // APIから画像を取得
   console.log("Home: 画像情報を取得しました", image);
   return <div>猫画像予定地</div>;
 }
